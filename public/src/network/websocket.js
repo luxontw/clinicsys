5ee5942a06e3b2f03e401fc19f1465e7f32e7fc8
luxontw <jeff85898@gmail.com>
2022-05-14 19:46:57 +0800

    fix: Socket url

--- public/src/network/websocket.js
+++ public/src/network/websocket.js
@@ -1,3 +1,2 @@
 import  {io} from "socket.io-client";
-export const socket = io("http://localhost:8888");
-//export const socket = io("https://clinicsys.azurewebsites.net/");
+export const socket = io("https://clinicsys.azurewebsites.net/");
